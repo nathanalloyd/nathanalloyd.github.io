a41f79e4dceaac867034995e19074c19f8640e1a
Nathan Lloyd <nathanalloyd@gmail.com>
2021-11-08 11:40:02 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Welcome to my portfolio page! This site will serve as a hub where I can upload ongoing and completed projects as well as update my programming language proficiencies.  The purpose of this site is to showcase my work in order to develop my online presence and inform potential employers of my qualifications.  
+
+Code Louisville project requirements are as follows: 
+
+Media queries begin on line 105 in the styles.css file. Media queries provide responsive layouts for both medium and large screens with the initial code being written from a mobile-first approach.  
+
+CSS features include the use of Flexbox to rearrange the content of my header buttons. These buttons are centered on a mobile viewport and placed on the right side on medium and large viewports. This can be viewed on line 3 and line 112 in the style.css file. 
+
+A second CSS feature is a CSS animation found on line 207 in the style.css file. This animation creates the illusion of zooming in or flying in to the planet. This did not look good on smaller viewports, so I created a breakpoint to include this only on larger screens.
+
+Javascript features include showing and hiding content when clicking the menu buttons. This content includes images depicting my character avatar, his speech bubble, and the visible content represented by each button. You can view this code in the images.js file. 
---
+++ javascript/images.js
@@ -0,0 +1,33 @@
+const biofunction = () => {
+    document.getElementById("bio").style.display ='block'
+    document.getElementById("skills").style.display ='none'
+    document.getElementById("projects").style.display ='none'
+    document.getElementById("avatar1").style.display ='none'
+    document.getElementById("avatar2").style.display ='block'
+    document.getElementById("avatar3").style.display ='none'
+    document.getElementById("avatar4").style.display ='none'
+}
+
+const skillsfunction = () => {
+    document.getElementById("skills").style.display ='block'
+    document.getElementById("bio").style.display ='none'
+    document.getElementById("projects").style.display ='none'
+    document.getElementById("avatar1").style.display ='none'
+    document.getElementById("avatar2").style.display ='none'
+    document.getElementById("avatar3").style.display ='block'
+    document.getElementById("avatar4").style.display ='none'
+}
+
+const projectsfunction = () => {
+    document.getElementById("projects").style.display ='block'
+    document.getElementById("bio").style.display ='none'
+    document.getElementById("skills").style.display ='none'
+    document.getElementById("avatar1").style.display ='none'
+    document.getElementById("avatar2").style.display ='none'
+    document.getElementById("avatar3").style.display ='none'
+    document.getElementById("avatar4").style.display ='block'
+}
+
+const resumefunction = () => {
+    document.getElementById("resume.docx")
+}
